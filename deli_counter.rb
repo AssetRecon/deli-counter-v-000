fdb39424113f4ec4e207de5939d5aeb8c6ca38a5
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 18:58:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line
+ if katz_deli.size == 0
+   puts "The line is currently empty."
+end
+
+def take_a_number(array,name)
+  array << name
+  puts name
+  puts array.index(name)+1
+
+end
+
