33d32186740589660cfebbcb1b4dfe4571768700
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 19:02:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,14 @@ katz_deli = []
 def line(array)
  if array.size == 0
    puts "The line is currently empty."
+ else
+
+   puts "The line is currently:"
    end
 end
 
 def take_a_number(array,name)
   array << name
-  puts name
-  puts array.index(name)+1
+  puts "Welcome #{name} you are number #{array.size} in line."
 
 end
