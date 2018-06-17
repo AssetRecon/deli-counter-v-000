1b072e31a400ecff20abda9bbf8672ac38140d16
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 19:07:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,16 @@
 katz_deli = []
 
 def line(array)
+  numbered_array = []
  if array.size == 0
    puts "The line is currently empty."
  else
-
-   puts "The line is currently:"
+   counter = 1
+array.each do |person|
+  numbered_array << "#{counter}. #{person}"
+  counter += 1
+    end
+   puts "The line is currently: #{array.join}"
    end
 end
 
